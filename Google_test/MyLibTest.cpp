40cec5a4c1e0b0f5b7419b1147814b977e8e44a1
Édouard <edouard@gomez-vaez.com>
2020-02-13 23:09:15 +0100

    Refactoring test code and renaming hello to gameoflife

--- Google_test/MyLibTest.cpp
+++ Google_test/MyLibTest.cpp
@@ -1,71 +1,54 @@
 #include "gtest/gtest.h"
 #include "mylib.h"
 
-const int H = 2;
-const int W = 3;
-
-void initializeWith(bool **world, bool initv[H][W]) {
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 3; j++) {
-      world[i][j] = initv[i][j];
-    }
-  }
-}
-
-TEST(MyLibTestSuite, AFirstTest)
-{
-  bool initv[2][3] = {{true,false,true},{false,true,false}};
-  bool expected[2][3] = {{false,true,false},{true,false,true}};
-
-  bool **world = new bool*[2];
-
-  for (int i=0; i < 2; i++) {
-    world[i] = new bool[3];
-  }
-
-  initializeWith(world, initv);
-
-  nextWorld(world, 2, 3);
-  for (int i=0; i < 2; i++) {
-    for (int j = 0; j < 3; j++)
-      ASSERT_EQ(world[i][j], expected[i][j]);
-  }
-}
-
-TEST(MyLibTestSuite, JustToSee) {
-  const int HEIGHT = 7;
-  const int WIDTH = 3;
-
-  int **world;
-
-  world = new int*[HEIGHT];
-
-  for (int i = 0; i < HEIGHT; i++) {
-    world[i] = new int[WIDTH];
-  }
-
-  for (int i = 0; i < HEIGHT; i++) {
-    for (int j = 0; j < WIDTH; j++) {
-      world[i][j] = (i + 2*j);
-    }
-  }
-
+void printfWorld(bool **world, int H, int W) {
   printf("\n");
-  for (int i = 0; i < HEIGHT; i++) {
-    for (int j = 0; j < WIDTH; j++) {
-      if (world[i][j] == 6) {
-        printf("youpee");
-      }
+  for (int i = 0; i < H; i++) {
+    for (int j = 0; j < W; j++) {
       printf("%i-", world[i][j]);
     }
     printf("\n");
   }
+}
 
-  ASSERT_EQ(1,2);
-
-  for (int i = 0; i < HEIGHT; i++) {
+void deleteWorld(bool **world, int H) {
+  for (int i = 0; i < H; i++) {
     delete world[i];
   }
-
   delete world;
+}
+
+void assertExpectedWorld(bool **expected, bool **world, int H, int W) {
+  for (int i=0; i < H; i++) {
+    for (int j = 0; j < W; j++) {
+      std::string world_v = std::to_string(i) + "," + std::to_string(j) + ": " + (world[i][j] ? "true" : "false");
+      std::string expected_v = std::to_string(i) + "," + std::to_string(j) + ": " + (expected[i][j] ? "true" : "false");
+      EXPECT_EQ(world_v, expected_v);
+    }
+  }
+}
+
+TEST(MyLibTestSuite, Die) {
+  int H = 2;
+  int W = 3;
+
+  bool **world = new bool *[H] {
+    new bool[W] { true, false, false },
+    new bool[W] { true, false, false }
+  };
+
+  bool **expected = new bool *[H] {
+      new bool[W]{ false, false, true },
+      new bool[W]{ false, false, false }
+  };
+
+  nextWorld(world, H, W);
+
+  assertExpectedWorld(expected, world, H, W);
+
+  printfWorld(world, H, W);
+
+  deleteWorld(world, H);
+  deleteWorld(expected, H);
+
 }
