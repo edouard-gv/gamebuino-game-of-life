ac6d2ee828f73dca7f2c73274793d38cafa07098
edouard <edouard@gomez-vaez.com>
2020-02-16 22:00:35 +0100

    Migrate indent to two spaces

--- Google_test/MyLibTest.cpp
+++ Google_test/MyLibTest.cpp
@@ -15,21 +15,21 @@ void printfWorld(bool **world, int h, int w) {
 }
 
 void printfNbNeighbours(bool **world, int h, int w) {
-    printf("\n");
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            printf("%i-", livingNeighbours(i, j, world, h, w));
-        }
-        printf("\n");
+  printf("\n");
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      printf("%i-", livingNeighbours(i, j, world, h, w));
     }
+    printf("\n");
+  }
 }
 
 bool ** createNewWorld(int h, int w) {
-    bool **newWorld  = new bool *[h];
-    for (int i = 0; i < h; ++i) {
-        newWorld[i] = new bool[w];
-    }
-    return newWorld;
+  bool **newWorld  = new bool *[h];
+  for (int i = 0; i < h; ++i) {
+    newWorld[i] = new bool[w];
+  }
+  return newWorld;
 }
 
 void deleteWorld(bool **world, int h) {
@@ -54,159 +54,159 @@ void assertExpectedWorld(bool **expected, bool **world, int h, int w) {
 std::string scoord(int i, int j) { return std::to_string(i) + "," + std::to_string(j) + ": "; }
 
 TEST(MyLibTestSuite, Die) {
-    int H = 3;
-    int W = 3;
+  int H = 3;
+  int W = 3;
 
-    bool **world = new bool *[H] {
-            new bool[W] { o, o, o },
-            new bool[W] { o, x, o },
-            new bool[W] { o, o, o }
-    };
+  bool **world = new bool *[H] {
+      new bool[W] { o, o, o },
+      new bool[W] { o, x, o },
+      new bool[W] { o, o, o }
+  };
 
-    bool **expected = new bool *[H] {
-            new bool[W]{ o, o, o },
-            new bool[W]{ o, o, o },
-            new bool[W]{ o, o, o }
-    };
+  bool **expected = new bool *[H] {
+      new bool[W]{ o, o, o },
+      new bool[W]{ o, o, o },
+      new bool[W]{ o, o, o }
+  };
 
-    bool **newWorld = createNewWorld(H, W);
-    nextWorld(world, newWorld, H, W);
-    assertExpectedWorld(expected, newWorld, H, W);
+  bool **newWorld = createNewWorld(H, W);
+  nextWorld(world, newWorld, H, W);
+  assertExpectedWorld(expected, newWorld, H, W);
 
-    deleteWorld(world, H);
-    deleteWorld(newWorld, H);
-    deleteWorld(expected, H);
+  deleteWorld(world, H);
+  deleteWorld(newWorld, H);
+  deleteWorld(expected, H);
 }
 
 TEST(MyLibTestSuite, Blink) {
-    int H = 4;
-    int W = 4;
+  int H = 4;
+  int W = 4;
 
-    bool **world = new bool *[H] {
-            new bool[W] { o, x, o, o },
-            new bool[W] { o, x, o, o },
-            new bool[W] { o, x, o, o },
-            new bool[W] { o, o, o, o },
-    };
+  bool **world = new bool *[H] {
+      new bool[W] { o, x, o, o },
+      new bool[W] { o, x, o, o },
+      new bool[W] { o, x, o, o },
+      new bool[W] { o, o, o, o },
+  };
 
-    bool **expected = new bool *[H] {
-            new bool[W] { o, o, o, o },
-            new bool[W] { x, x, x, o },
-            new bool[W] { o, o, o, o },
-            new bool[W] { o, o, o, o },
-    };
+  bool **expected = new bool *[H] {
+      new bool[W] { o, o, o, o },
+      new bool[W] { x, x, x, o },
+      new bool[W] { o, o, o, o },
+      new bool[W] { o, o, o, o },
+  };
 
-    bool **newWorld = createNewWorld(H, W);
-    nextWorld(world, newWorld, H, W);
-    assertExpectedWorld(expected, newWorld, H, W);
+  bool **newWorld = createNewWorld(H, W);
+  nextWorld(world, newWorld, H, W);
+  assertExpectedWorld(expected, newWorld, H, W);
 
-    deleteWorld(world, H);
-    deleteWorld(newWorld, H);
-    deleteWorld(expected, H);
+  deleteWorld(world, H);
+  deleteWorld(newWorld, H);
+  deleteWorld(expected, H);
 }
 
 TEST(MyLibTestSuite, OverPopulation) {
-    int H = 4;
-    int W = 4;
+  int H = 4;
+  int W = 4;
 
-    bool **world = new bool *[H] {
-            new bool[W] { x, x, o, o },
-            new bool[W] { x, x, o, o },
-            new bool[W] { x, x, o, o },
-            new bool[W] { o, o, o, o },
-    };
+  bool **world = new bool *[H] {
+      new bool[W] { x, x, o, o },
+      new bool[W] { x, x, o, o },
+      new bool[W] { x, x, o, o },
+      new bool[W] { o, o, o, o },
+  };
 
-    bool **expected = new bool *[H] {
-            new bool[W] { x, x, o, o },
-            new bool[W] { o, o, x, x },
-            new bool[W] { x, x, o, o },
-            new bool[W] { o, o, o, o },
-    };
+  bool **expected = new bool *[H] {
+      new bool[W] { x, x, o, o },
+      new bool[W] { o, o, x, x },
+      new bool[W] { x, x, o, o },
+      new bool[W] { o, o, o, o },
+  };
 
-    bool **newWorld = createNewWorld(H, W);
-    nextWorld(world, newWorld, H, W);
-    assertExpectedWorld(expected, newWorld, H, W);
+  bool **newWorld = createNewWorld(H, W);
+  nextWorld(world, newWorld, H, W);
+  assertExpectedWorld(expected, newWorld, H, W);
 
-    deleteWorld(world, H);
-    deleteWorld(newWorld, H);
-    deleteWorld(expected, H);
+  deleteWorld(world, H);
+  deleteWorld(newWorld, H);
+  deleteWorld(expected, H);
 }
 
 TEST(MyLibTestSuite, allDyings) {
-    int H = 3;
-    int W = 3;
+  int H = 3;
+  int W = 3;
 
-    bool **world = new bool *[H] {
-            new bool[W] { o, o, o },
-            new bool[W] { o, x, o },
-            new bool[W] { o, o, o }
-    };
+  bool **world = new bool *[H] {
+      new bool[W] { o, o, o },
+      new bool[W] { o, x, o },
+      new bool[W] { o, o, o }
+  };
 
-    EXPECT_EQ(livingNeighbours(1, 1, world, H, W), 0);
+  EXPECT_EQ(livingNeighbours(1, 1, world, H, W), 0);
 
-    deleteWorld(world, H);
+  deleteWorld(world, H);
 }
 
 TEST(MyLibTestSuite, oneSurvives) {
-    int H = 5;
-    int W = 5;
+  int H = 5;
+  int W = 5;
 
-    bool **world = new bool *[H] {
-            new bool[W] { o, o, o, o, o },
-            new bool[W] { o, o, o, o, o },
-            new bool[W] { o, o, x, o, o },
-            new bool[W] { o, o, o, o, o },
-            new bool[W] { o, o, o, o, o }
-    };
+  bool **world = new bool *[H] {
+      new bool[W] { o, o, o, o, o },
+      new bool[W] { o, o, o, o, o },
+      new bool[W] { o, o, x, o, o },
+      new bool[W] { o, o, o, o, o },
+      new bool[W] { o, o, o, o, o }
+  };
 
-    for (int i = 1; i < 4; ++i) {
-        for (int j = 1; j < 4; ++j) {
-            EXPECT_EQ(scoord(i, j) + std::to_string(livingNeighbours(i, j, world, H, W)),
-                      scoord(i, j) + std::to_string(i==2 && j==2 ? 0 : 1));
-        }
+  for (int i = 1; i < 4; ++i) {
+    for (int j = 1; j < 4; ++j) {
+      EXPECT_EQ(scoord(i, j) + std::to_string(livingNeighbours(i, j, world, H, W)),
+                scoord(i, j) + std::to_string(i==2 && j==2 ? 0 : 1));
     }
+  }
 
-    deleteWorld(world, H);
+  deleteWorld(world, H);
 }
 
 TEST(MyLibTestSuite, livingOnTheBorders) {
-    int H = 3;
-    int W = 3;
+  int H = 3;
+  int W = 3;
 
-    bool **world = new bool *[H] {
-            new bool[W] { o, o, o },
-            new bool[W] { o, o, x },
-            new bool[W] { o, o, o }
-    };
+  bool **world = new bool *[H] {
+      new bool[W] { o, o, o },
+      new bool[W] { o, o, x },
+      new bool[W] { o, o, o }
+  };
 
-    for (int i = 0; i < H; ++i) {
-        for (int j = 0; j < W; ++j) {
-            EXPECT_EQ(scoord(i, j) + std::to_string(livingNeighbours(i, j, world, H, W)),
-                      scoord(i, j) + std::to_string(i==1 && j==2 ? 0 : 1));
-        }
+  for (int i = 0; i < H; ++i) {
+    for (int j = 0; j < W; ++j) {
+      EXPECT_EQ(scoord(i, j) + std::to_string(livingNeighbours(i, j, world, H, W)),
+                scoord(i, j) + std::to_string(i==1 && j==2 ? 0 : 1));
     }
+  }
 
-    deleteWorld(world, H);
+  deleteWorld(world, H);
 }
 
 TEST(MyLibTestSuite, duplicate) {
-    bool **world = new bool *[1]{
-        new bool[1] {true}
-    };
+  bool **world = new bool *[1]{
+      new bool[1] {true}
+  };
 
-    bool **savedWorld = new bool *[1]{
-            new bool[1] {false}
-    };
+  bool **savedWorld = new bool *[1]{
+      new bool[1] {false}
+  };
 
-    copyWorld(world, savedWorld, 1, 1);
+  copyWorld(world, savedWorld, 1, 1);
 
-    EXPECT_EQ(true, savedWorld[0][0]);
+  EXPECT_EQ(true, savedWorld[0][0]);
 
-    world[0][0] =  false;
+  world[0][0] =  false;
 
-    EXPECT_EQ(true, savedWorld[0][0]);
+  EXPECT_EQ(true, savedWorld[0][0]);
 }
 
 TEST(MyLibTestSuite, play) {
-    EXPECT_EQ(-1 % 4, -1);
+  EXPECT_EQ(-1 % 4, -1);
 }
--- MyLib_lib/mylib.h
+++ MyLib_lib/mylib.h
@@ -10,43 +10,43 @@ int livingNeighbours(int i, int j, bool **world, int h, int w);
 void initializeWorld(bool **world, int h, int w);
 
 void nextWorld(bool **world, bool **newWorld, int h, int w) {
-    for (int i = 0; i < h; ++i) {
-        for (int j = 0; j < w; ++j) {
-            int an = livingNeighbours(i, j, world, h, w);
-            if (an < 2 || an > 3)
-                newWorld[i][j] = false;
-            else if (an == 3)
-                newWorld[i][j] = true;
-            else
-                newWorld[i][j] = world[i][j];
-        }
+  for (int i = 0; i < h; ++i) {
+    for (int j = 0; j < w; ++j) {
+      int an = livingNeighbours(i, j, world, h, w);
+      if (an < 2 || an > 3)
+        newWorld[i][j] = false;
+      else if (an == 3)
+        newWorld[i][j] = true;
+      else
+        newWorld[i][j] = world[i][j];
     }
+  }
 }
 
 int livingNeighbours(int i, int j, bool **world, int h, int w) {
-    int livings = 0;
-    for (int k = i-1; k <= i+1; ++k) {
-        for (int l = j-1; l <= j+1 ; ++l) {
-            if (!(k==i && l==j) && world[(k + h) % h][(l + w) % w]) livings++;
-        }
+  int livings = 0;
+  for (int k = i-1; k <= i+1; ++k) {
+    for (int l = j-1; l <= j+1 ; ++l) {
+      if (!(k==i && l==j) && world[(k + h) % h][(l + w) % w]) livings++;
     }
-    return livings;
+  }
+  return livings;
 }
 
 void initializeWorld(bool **world, int h, int w) {
-    for (int i=0; i < h; i++) {
-        for (int j=0; j < w; j++) {
-            world[i][j] = (rand() % 3 == 0);
-        }
+  for (int i=0; i < h; i++) {
+    for (int j=0; j < w; j++) {
+      world[i][j] = (rand() % 3 == 0);
     }
+  }
 }
 
 void copyWorld(bool **source, bool **target, int h, int w) {
-    for (int i=0; i < h; i++) {
-        for (int j = 0; j < w; ++j) {
-            target[i][j] = source[i][j];
-        }
+  for (int i=0; i < h; i++) {
+    for (int j = 0; j < w; ++j) {
+      target[i][j] = source[i][j];
     }
+  }
 }
 
 #endif //HELLO_MYLIB_H
